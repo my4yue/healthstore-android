781bd588db6fa15afbeeb71e2dbc3a19a96b428b
my4yue <my4yue@126.com>
2018-06-21 22:58:16 +0800

    add butterknife

--- app/src/main/java/com/healthstore/app/MainActivity.java
+++ app/src/main/java/com/healthstore/app/MainActivity.java
@@ -2,13 +2,27 @@ package com.healthstore.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+//    @BindView(R.id.btn_test_1) Button btnTest1;
+//    @BindView(R.id.btn_test_2) Button btnTest2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
     }
+
+    @OnClick({R.id.btn_test_1, R.id.btn_test_2})
+    void onClickBtn(Button btn){
+        btn.setText(btn.getId()+"");
+    }
+
 }

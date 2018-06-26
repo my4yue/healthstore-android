c5a8b3a3c8394d8d6fd93e04b6be8abc01503cd6
mayue <mayue@techfantasy.com.cn>
2018-06-26 17:39:50 +0800

    反馈页面和个人信息页

--- app/src/main/java/com/healthstore/app/view/MainActivity.java
+++ app/src/main/java/com/healthstore/app/view/MainActivity.java
@@ -1,20 +1,41 @@
 package com.healthstore.app.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.healthstore.app.R;
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
 import com.qmuiteam.qmui.widget.QMUITopBar;
+import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    QMUITopBar topBar;
+    @BindView(R.id.top_bar) QMUITopBarLayout topBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
+        ButterKnife.bind(this);
+
+        topBar.setTitle("反馈");
+        topBar.addLeftBackImageButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("a", "onClick");
+                Toast.makeText(MainActivity.this, "后退", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, MeActivity.class));
+            }
+        });
+        QMUIStatusBarHelper.translucent(this);
+
     }
 
 }
---
+++ app/src/main/java/com/healthstore/app/view/MeActivity.java
@@ -0,0 +1,50 @@
+package com.healthstore.app.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.healthstore.app.R;
+import com.qmuiteam.qmui.util.QMUIStatusBarHelper;
+import com.qmuiteam.qmui.widget.QMUIRadiusImageView;
+import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+import com.qmuiteam.qmui.widget.grouplist.QMUICommonListItemView;
+import com.qmuiteam.qmui.widget.grouplist.QMUIGroupListView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MeActivity extends AppCompatActivity {
+
+    @BindView(R.id.top_bar) QMUITopBarLayout topBar;
+    @BindView(R.id.icon) QMUIRadiusImageView icon;
+    @BindView(R.id.me_list) QMUIGroupListView meListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me);
+        ButterKnife.bind(this);
+
+        topBar.setTitle("个人信息");
+        topBar.addLeftBackImageButton();
+
+        QMUICommonListItemView iv = meListView.createItemView("昵称");
+        iv.setDetailText("风之幽水");
+        iv.setOrientation(QMUICommonListItemView.HORIZONTAL);
+
+        QMUIGroupListView.newSection(this)
+                .addItemView(iv, new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MeActivity.this, "我要改昵称", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addItemView(meListView.createItemView("性别"), null)
+                .addItemView(meListView.createItemView("地区"), null).addTo(meListView);
+
+        QMUIStatusBarHelper.translucent(this);
+
+    }
+}

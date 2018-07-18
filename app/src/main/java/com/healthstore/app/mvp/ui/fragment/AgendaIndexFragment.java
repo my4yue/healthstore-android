a6ce0f18a451c4de9a8306fa2dd944906c0eef0b
my4yue <my4yue@126.com>
2018-07-19 00:12:14 +0800

    popup window

--- app/src/main/java/com/healthstore/app/mvp/ui/fragment/AgendaIndexFragment.java
+++ app/src/main/java/com/healthstore/app/mvp/ui/fragment/AgendaIndexFragment.java
@@ -1,31 +1,46 @@
 package com.healthstore.app.mvp.ui.fragment;
 
-import android.arch.lifecycle.Observer;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.healthstore.app.R;
 import com.healthstore.app.di.component.AppComponent;
 import com.healthstore.app.di.component.DaggerAgendaComponent;
 import com.healthstore.app.di.module.AgendaModule;
 import com.healthstore.app.mvp.contract.AgendaContract;
-import com.healthstore.app.mvp.model.entity.User;
 import com.healthstore.app.mvp.presenter.AgendaPresenter;
+import com.healthstore.app.utils.DateUtils;
+import com.qmuiteam.qmui.util.QMUIDisplayHelper;
 import com.qmuiteam.qmui.widget.QMUIProgressBar;
 import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+import com.qmuiteam.qmui.widget.popup.QMUIPopup;
 
-import javax.inject.Inject;
+import java.util.Date;
 
 import butterknife.BindView;
 
+import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
+
 public class AgendaIndexFragment extends AppFragment<AgendaPresenter> implements AgendaContract.View {
 
-    @BindView(R.id.agenda_bg) ImageView agendaBackground;
-    @BindView(R.id.top_bar) QMUITopBarLayout topBar;
-    @BindView(R.id.progress_bar) QMUIProgressBar progressBar;
+    @BindView(R.id.agenda_bg)
+    ImageView agendaBackground;
+    @BindView(R.id.top_bar)
+    QMUITopBarLayout topBar;
+    @BindView(R.id.progress_bar)
+    QMUIProgressBar progressBar;
+    @BindView(R.id.tv_date)
+    TextView tvDate;
+    @BindView(R.id.tv_weekday)
+    TextView tvWeekDay;
 
     @Override
     int layoutResId() {
@@ -44,14 +59,28 @@ public class AgendaIndexFragment extends AppFragment<AgendaPresenter> implements
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
         topBar.setTitle("日程");
-        topBar.addRightTextButton("+", 0);
-
-        progressBar.setQMUIProgressBarTextGenerator(new QMUIProgressBar.QMUIProgressBarTextGenerator() {
-            @Override public String generateText(QMUIProgressBar progressBar, int value, int maxValue) {
-                return (value * 100 / maxValue) + "%";
-            }
+        Drawable drawable = ContextCompat.getDrawable(getActivity(), android.R.drawable.ic_input_add);
+        topBar.addRightImageButton(android.R.drawable.ic_input_add, R.id.btn_agenda_index_add).setOnClickListener(v -> {
+            QMUIPopup popup = new QMUIPopup(getActivity(), QMUIPopup.DIRECTION_BOTTOM);
+//            popup.setPopupLeftRightMinMargin(-20);
+            popup.setPositionOffsetX(100);
+//            popup.setPositionOffsetYWhenBottom(100);
+            TextView textView = new TextView(getContext());
+            textView.setLayoutParams(popup.generateLayoutParam(WRAP_CONTENT, WRAP_CONTENT));
+//            textView.setLineSpacing(QMUIDisplayHelper.dp2px(getContext(), 4), 1.0f);
+            int padding = QMUIDisplayHelper.dp2px(getContext(), 20);
+            textView.setPadding(padding, padding, padding, padding);
+            textView.setText("添加提醒");
+            textView.setTextColor(Color.BLACK);
+            popup.setContentView(textView);
+            popup.setAnimStyle(QMUIPopup.ANIM_GROW_FROM_RIGHT);
+            popup.show(v);
         });
+
+        progressBar.setQMUIProgressBarTextGenerator((progressBar, value, maxValue) -> (value * 100 / maxValue) + "%");
         progressBar.setProgress(90);
+        tvDate.setText(DateUtils.formatDate(new Date(), DateUtils.sdfDateCn));
+        tvWeekDay.setText(DateUtils.getWeekDay(new Date()).getDescCn());
 
         mAppManager.getMainUser().observe(this, user -> mImageLoader.loadPicture(getContext(), user.getAgendaBackgroundImageUrl(), agendaBackground));
     }
--- app/src/main/java/com/healthstore/app/utils/DateUtils.java
+++ app/src/main/java/com/healthstore/app/utils/DateUtils.java
@@ -1,14 +1,56 @@
 package com.healthstore.app.utils;
 
 import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtils {
 
-    private static final SimpleDateFormat sdfDateTime = new SimpleDateFormat("yyyy-MM-dd+hh:mm:ss");
+    public static final SimpleDateFormat sdfDateTime = new SimpleDateFormat("yyyy-MM-dd+hh:mm:ss");
+    public static final SimpleDateFormat sdfDateCn = new SimpleDateFormat("yyyy年MM月dd日");
 
-    public static String formatNow(){
+    public enum DayOfWeek {
+        Sunday("星期日", Calendar.SUNDAY),
+        Monday("星期一", Calendar.MONDAY),
+        Tuesday("星期二", Calendar.TUESDAY),
+        Wednesday("星期三", Calendar.WEDNESDAY),
+        Thursday("星期四", Calendar.THURSDAY),
+        Friday("星期五", Calendar.FRIDAY),
+        Saturday("星期六", Calendar.SATURDAY);
+
+        String descCn;
+        int val;
+
+        DayOfWeek(String descCn, int val) {
+            this.descCn = descCn;
+            this.val = val;
+        }
+
+        public String getDescCn(){
+            return descCn;
+        }
+
+        public static DayOfWeek of(int val) {
+            return Arrays.stream(DayOfWeek.values()).filter(dayOfWeek -> dayOfWeek.val==val).findFirst().orElse(null);
+        }
+
+    }
+
+    public static String formatNow() {
         return sdfDateTime.format(new Date());
+    }
+
+    public static String formatDate(Date date, SimpleDateFormat sdf) {
+        return sdf.format(date);
+    }
+
+    public static DayOfWeek getWeekDay(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        return DayOfWeek.of(dayOfWeek);
     }
 
 }
